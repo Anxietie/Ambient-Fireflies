af73662ee67338149e29bbbb01857183312a92e6
Anxietie <84636109+Anxietie@users.noreply.github.com>
2024-02-19 10:40:05 -0500

    init

---
+++ src/client/java/com/mod/AmbientFirefliesClient.java
@@ -0,0 +1,10 @@
+package com.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AmbientFirefliesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
